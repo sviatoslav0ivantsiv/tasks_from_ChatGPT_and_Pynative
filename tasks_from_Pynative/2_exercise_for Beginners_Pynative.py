b1014b3f5f2adcfde58f45191ecbb4e8c1fae736
Sviatik <sviatik.ivantsiv@gmail.com>
2025-11-04 18:10:37 +0200

    Add exercise 1-8 from Chat adn 2 from Pynetive

---
+++ tasks_from_Pynative/2_exercise_for Beginners_Pynative.py
@@ -0,0 +1,11 @@
+# # Start of Exercise 1
+# previous_num = 0
+#
+# for Current_Number in range(10):
+#     sum = Current_Number + previous_num
+#     print("Current Number:", Current_Number, "Previous Number", previous_num, "Sum:", sum)
+#     previous_num = Current_Number
+# # End of Exercise 1
+
+
+
