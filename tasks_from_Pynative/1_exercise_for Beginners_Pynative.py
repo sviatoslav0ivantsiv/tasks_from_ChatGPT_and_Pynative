4b729845552c12547c09024c9289bcd4c0ab4c2a
Sviatik <sviatik.ivantsiv@gmail.com>
2025-11-25 08:17:48 +0200

    Add : 3_exercise_for Beginners_Pynative

--- tasks_from_Pynative/1_exercise_for Beginners_Pynative.py
+++ tasks_from_Pynative/1_exercise_for Beginners_Pynative.py
@@ -1,11 +1,11 @@
-# Start of Exercise 1
-number1 = 40
-number2 = 30
-
-multiply = number1 * number2
-add = number1 + number2
-if multiply <= 1000:
-    print(multiply)
-else:
-    print(add)
-# End of Exercise 1
+# # Start of Exercise 1
+# number1 = 40
+# number2 = 30
+#
+# multiply = number1 * number2
+# add = number1 + number2
+# if multiply <= 1000:
+#     print(multiply)
+# else:
+#     print(add)
+# # End of Exercise 1
---
+++ tasks_from_Pynative/3_exercise_for Beginners_Pynative.py
@@ -0,0 +1,12 @@
+#  Start of Exercise 1
+import numbers
+
+# str = "PYnative"
+# consonants  = ""
+#
+# for letter in str:
+#     if letter.lower() not in 'aeiou':
+#         consonants += letter
+#         print (consonants)
+# for letter in range(0, 10, 2):
+#     print(letter)
