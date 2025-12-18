9eb3f50a7c6d374ab3c7813457e943d2547631dd
Sviatik <sviatik.ivantsiv@gmail.com>
2025-12-19 00:42:36 +0200

    Fix: Pynative Functions

--- tasks_from_Pynative/Pynative Functions.py
+++ tasks_from_Pynative/Pynative Functions.py
@@ -119,8 +119,111 @@
 
 # pynative Func Exercise 11
 # name="Alice", age=30
-def discribe_person(**discribe):
-    print("Name: ", discribe["name"], ", age: ", discribe["age"])
+# def discribe_person(**discribe):
+#     if discribe:
+#         print ("\n--- Information ---")
+#         for key, value in discribe.items():
+#             print(f"{key}: {value}")
+#     else:
+#             print ("\nNo information provided.")
+#
+# discribe_person(name="alisa", age=25, hair_color="blond")
+# discribe_person(job="enginer", hobby="running")
+#
+# discribe_person()
 
-discribe_person(name="Alice", age=30)
-# Didnot end
+
+# pynative Func Exercise 12
+
+# global_var = 10
+#
+# def new_global_var(number):
+#     global global_var
+#     global_var = number
+#     print("Inside function:", global_var)
+#
+#
+# new_global_var(15)
+#
+# print("Outside function:", global_var)
+
+
+# pynative Func Exercise 13: Write a recursive function to calculate the factorial
+
+# def factorial(n):
+#     if n == 0 or n == 1:
+#         return 1
+#     else:
+#         return n * factorial(n - 1)
+#
+# print(factorial(3))
+
+
+# pynative Func Exercise 14: Create a lambda function that squares a given number
+
+# def square(number):
+#
+#     result = lambda number: number * number
+#     print(result(number))
+#
+#
+# square(3)
+
+# pynative Func Exercise 15: Use a lambda with the filter() function to get all even numbers from a list
+
+# def even_number(numbers):
+#     even_numbers_list = []
+#     for number in numbers:
+#         if number % 2 == 0:
+#             even_numbers_list.append(number)
+#     return even_numbers_list
+#
+# print("the even number list are: ", even_number([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]))
+
+# numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# even_numbers = list(filter(lambda number: number % 2 == 0, numbers))
+#
+# print(even_numbers)
+
+
+# pynative Func Exercise 16: Use a lambda with the map() function to double each element in a list
+
+# numbers = [1, 2, 3, 4, 5]
+# double_numbers = list(map(lambda number: number *2, numbers))
+# print(double_numbers)
+
+# pynative Func Exercise 17: Use a lambda with the sorted() function to sort a list of tuples based on the second element
+
+# data = [('apple', 5), ('banana', 2), ('cherry', 8), ('date', 1)]
+# sorted_data = sorted(data, key=lambda item: item[1])
+# print(sorted_data)
+
+
+# pynative Func Exercise 18: Create Higher-Order Function
+
+def apply_operation(func, x, y):
+    return func(x, y)
+
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    return x / y
+
+result_add = apply_operation(add, 2, 4)
+print("result of add:", result_add)
+
+result_subtract = apply_operation(subtract, 2, 4)
+print(result_subtract)
+
+result_multiply = apply_operation(multiply, 2, 4)
+print(result_multiply)
+
+result_divide = apply_operation(divide, 2, 4)
+print(result_divide)
