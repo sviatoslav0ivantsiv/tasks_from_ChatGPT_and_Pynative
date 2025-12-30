6fbe0974378a19eca8375a39f2057537cadd623d
Sviatik <sviatik.ivantsiv@gmail.com>
2025-12-30 09:16:13 +0200

    Add: Pynative OOP Exercise

---
+++ tasks_from_Pynative/Pynative_OOP.py
@@ -0,0 +1,47 @@
+# pynative OOP Exercise 1:
+#
+# class Vehicle :
+#     def __init__(self, max_speed, mileage ):
+#         self.max_speed = max_speed
+#         self.mileage = mileage
+#
+# modelS1 = Vehicle(300, 200)
+#
+# print(modelS1.max_speed, modelS1.mileage)
+
+# pynative OOP Exercise 2:
+# class Vehicle:
+#     pass
+
+# pynative OOP Exercise 3:
+# class Vehicle:
+#
+#     def __init__(self, name, max_speed, mileage):
+#         self.name = name
+#         self.max_speed = max_speed
+#         self.mileage = mileage
+#
+# class Bus(Vehicle):
+#
+#     def print_info(self):
+#         print( f"Vehicle is {self.name}, his max speed is {self.max_speed}, and mileage is {self.mileage}")
+# SchoolBus = Bus("School Volvo", 180, 12)
+# SchoolBus.print_info()
+
+# pynative OOP Exercise 4: Class Inheritance:
+# class Vehicle:
+#     def __init__(self, name, max_speed, mileage):
+#         self.name = name
+#         self.max_speed = max_speed
+#         self.mileage = mileage
+#
+#     def seating_capacity(self, capacity):
+#         return f"The seating capacity of a {self.name} is {capacity} passengers"
+# # SameVehicle = Vehicle("Same Vehicle", 40000, 1000)
+# # print(SameVehicle.seating_capacity(100))
+# class Bus(Vehicle):
+#     def seating_capacity(self, capacity = 50):
+#         return f"The seating capacity of a {self.name} is {capacity} passengers"
+#
+# SameBus = Bus("Bus", 180, 12)
+# print(SameBus.seating_capacity())
